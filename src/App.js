eb55ddc67c4475b5e6e56ade085c635ca30f1f33
svanka024 <1196688@student.roc-nijmegen.nl>
2023-06-21 16:00:25 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Signup from "./componments/Signup";
+
+import React, { useEffect, useState } from "react";
+
+import {getDocs, collection, doc} from 'firebase/firestone';
+import { db } from '../firebase-config';
+
+const [getTaskList, setTaskList] = useState([]);
+  const jobCollectionRef = collection(db, "tasks");
+
+  useEffect(()=>{
+    const getTasks = async () => {
+      const data = await getDocs(jobCollectionRef);
+      setTaskList(data.docs.map((docs) => ({...doc.data(), id: doc.id})));
+    }
+    getTasks();
+  },[])
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Signup />
     </div>
   );
 }
