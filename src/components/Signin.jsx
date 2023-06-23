e902594168e9eab6db9ef8deaf692da295c81825
jaspervanuden <jaspervanuden03@gmail.com>
2023-06-23 09:50:15 +0200

    update

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -24,9 +24,9 @@ const Signin = () => {
   return (
     <div className='max-w-[700px] mx-auto my-16 p-4'>
       <div>
-        <h1 className='text-2xl font-bold py-2'>Sign in to your account</h1>
+        <h1 className='text-2xl font-bold py-2'>login</h1>
         <p className='py-2'>
-          Don't have an account yet?{' '}
+          Nog geen account?{' '}
           <Link to='/signup' className='underline'>
             Sign up.
           </Link>
@@ -34,7 +34,7 @@ const Signin = () => {
       </div>
       <form onSubmit={handleSubmit}>
         <div className='flex flex-col py-2'>
-          <label className='py-2 font-medium'>Email Address</label>
+          <label className='py-2 font-medium'>Email Addres</label>
           <input onChange={(e) => setEmail(e.target.value)} className='border p-3' type='email' />
         </div>
         <div className='flex flex-col py-2'>
@@ -42,7 +42,7 @@ const Signin = () => {
           <input onChange={(e) => setPassword(e.target.value)} className='border p-3' type='password' />
         </div>
         <button className='border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white'>
-          Sign In
+          Login 
         </button>
       </form>
     </div>
